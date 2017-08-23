73ad146b7eee1ef93bd5074dc60c8fe55147d533
Carlos Antonio Rodrigues <carlos.arodrigues@gmail.com>
2017-08-23 18:52:43 -0300

    Improving

--- src/test/java/com/sargentdisc/standalone/ReadWriteFileControllerTest.java
+++ src/test/java/com/sargentdisc/standalone/ReadWriteFileControllerTest.java
@@ -1,20 +1,20 @@
 package com.sargentdisc.standalone;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.io.File;
 import java.nio.file.Files;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sargentdisc.domain.model.userfile.UserFile;
+import com.sargentdisc.standalone.service.StandaloneService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,20 +23,19 @@ public class ReadWriteFileControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
+	
+	@Autowired
+	StandaloneService service;
 
 	@Test
-	public void uploadFile() throws Exception {
+	public void upload_file_without_commas_with_spaces() throws Exception {
 
 		String fileName = "DocumentTest.txt";
 		ClassLoader classLoader = this.getClass().getClassLoader();
 		File file = new File(classLoader.getResource(fileName).getFile());
-
-		MockMultipartFile fileMock = new MockMultipartFile("DocumentTest.txt", Files.readAllBytes(file.toPath()));
-
-		mvc.perform(MockMvcRequestBuilders.fileUpload("http://localhost:8081/standalone/uploadFile")
-				.file(fileMock)
-				.contentType(MediaType.MULTIPART_FORM_DATA))
-				.andExpect(status().isCreated());
+		UserFile userFile = new ObjectMapper().readValue(new String(Files.readAllBytes(file.toPath())), UserFile.class);
+		UserFile newFile = service.processFile(userFile);
+		Assert.assertNotNull(newFile);
 	}
 
 }
